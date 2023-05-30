1bc94a251455f6dd8d7fcd45a8d750a6053309d4
ekosribu <eko@sribu.com>
2023-05-30 14:03:46 +0700

    Update Global Variabel untuk profile freelancer, client, admin jarvis dan URL jarvis

---
+++ Object Repository/THANOS/Halaman Preview Job Detail/JudulJob.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>JudulJob</name>
+   <tag></tag>
+   <elementGuidId>a78994ea-522c-4a60-865a-1826a48cd72f</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
